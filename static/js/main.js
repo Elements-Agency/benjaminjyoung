fa9016c8dab146d525650d818178388e738722b9
Benjamin J. Young <blaher+github@pm.me>
2025-04-22 11:40:34 -0400

    Adding index engaged script

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,17 @@
-/*
-	Story by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+function trackEngaged() {
+	if (localStorage.engaged) {
+		return;
+	}
+
+	fbq('trackCustom', 'Engaged');
+	rdt('track', 'Custom', { customEventName: 'Engaged' });
+	pintrk('track', 'engagement');
+	gtag('event', 'engaged', {});
+
+	fbq('trackCustom', 'EngagedIndex');
+	
+	localStorage.engaged = true;
+}
 
 (function($) {
 
@@ -336,6 +345,11 @@
 
 							}, 275);
 
+						});
+
+	
+						$('a, button, input').click(function() {
+							trackEngaged();
 						});
 
 })(jQuery);
