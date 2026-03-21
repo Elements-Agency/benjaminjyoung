ba1223abf4989987c592a62cd9edfd057c372376
Benjamin J. Young <ben@blaher.me>
2026-03-21 06:43:15 -0400

    remove utm

--- assets/js/utms.js
+++ assets/js/utms.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const shopUtmUrl = buildUTMUrl('/shop/');
 
     // replace any hrefs to /shop/, /artwork/, or /story/ with UTM version
-    document.querySelectorAll('a[href*="/shop/"], a[href*="/artwork/"], a[href*="/story/"]').forEach(link => {
+    /*document.querySelectorAll('a[href*="/shop/"], a[href*="/artwork/"], a[href*="/story/"]').forEach(link => {
         link.href = buildUTMUrl(link.href);
-    });
+    });*/
 });
